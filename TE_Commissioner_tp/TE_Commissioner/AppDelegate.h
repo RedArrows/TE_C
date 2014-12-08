edc3835794feff37ded3f48edb2727bd4563bfbd
RedArrows <shiyao1990@126.com>
2014-12-08 13:50:44 +0800

    reject

---
+++ TE_Commissioner_tp/TE_Commissioner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TE_Commissioner
+//
+//  Created by shiyao on 14-12-4.
+//  Copyright (c) 2014年 shiyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
